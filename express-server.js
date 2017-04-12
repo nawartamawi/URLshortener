977557284f0d4f71b25e0116a58fcff9eb687377
nawartamawi <nawartamawi@gmail.com>
2017-04-12 20:25:51 +0000

    refactored the code and added comments

--- express-server.js
+++ express-server.js
@@ -1,23 +1,41 @@
+//adding the express library
 const express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080;
 
+//Specify what Port to use
+var PORT = process.env.PORT || 8080;
+//Import the random string Genrator
+const generateRandomString = require("./generateRandomString");
+
+//adding the middleware bodyparser library
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Specify the engine to render pages (EJS)
 app.set("view engine", "ejs");
+
+//our in memory db
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+//redirecting to /urls when requesting /
+app.get('/', (req, res) => {
+  res.redirect('/urls');
+});
 
+//Render urls_index.ejs to an HTML page when getting a GET request of root path of ./urls
 app.get('/urls', (req, res) => {
   let templateVars = {url: urlDatabase};
-  res.render("urls_index.ejs", templateVars);
+  res.render("urls_index", templateVars);
 });
+
+//Render urls_new to an HTML page when getting a GET request of root path of ./urls/new
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+//Render urls_show to an HTML page when getting a GET request with the shortURL embedded in the URL like locahost/urls/:nd33nz
 app.get('/urls/:id', (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
@@ -27,11 +45,25 @@ app.get('/urls/:id', (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  
-  res.send("ok");
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = "http://"+longURL;
+// redirect to the url/shorturl
+  res.redirect('/urls/' + shortURL);
 });
 
-
+app.get("/u/:shortURL", (req, res) => {
+  // let longURL = ...
+  // res.send(req.params.shortURL);
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  if (longURL === undefined) {
+    res.status(404).send(`${shortURL} is not found`);
+    return;
+  }
+//redirect to the long url found in DB
+  res.redirect(longURL);
+});
 
 
 //Keeping the server running 
